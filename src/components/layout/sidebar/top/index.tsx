f2e6c3e57f069ec28177ad3cbb6c96134ccf9955
Nofar Baranes <nofar@reallygood.co.il>
2020-06-23 16:46:24 +0300

    replacing sidebar top separator with the antd's Divider component

--- src/components/layout/sidebar/top/index.tsx
+++ src/components/layout/sidebar/top/index.tsx
@@ -1,15 +1,18 @@
 import React, { ReactElement } from 'react';
+import { Divider } from 'antd';
+
 import { ReactComponent as LogoIcon } from '../../../../images/logo.svg';
 
 import './index.scss';
 
 export default function Top(): ReactElement {
-    return (
-        <div className="top">
-            <div className="logo">
-                <span>TEACH</span>
-                <LogoIcon />
-            </div>
-        </div>
-    );
+  return (
+    <div className="top">
+      <div className="logo">
+        <span>TEACH</span>
+        <LogoIcon />
+      </div>
+      <Divider className="top-separator" />
+    </div>
+  );
 }
