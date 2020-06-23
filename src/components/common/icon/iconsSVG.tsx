5dba6649ce81d275ed787904bb2425d8667824df
Nofar Baranes <nofar@reallygood.co.il>
2020-06-23 16:38:46 +0300

    Replacing user-avatar svg icon

--- src/components/common/icon/iconsSVG.tsx
+++ src/components/common/icon/iconsSVG.tsx
@@ -2,7 +2,7 @@ import { IconType } from './icon.interface';
 
 import { ReactComponent as CoursesIcon } from '../../../images/icons/sidebar-courses.svg';
 import { ReactComponent as UsersIcon } from '../../../images/icons/sidebar-users.svg';
-import { ReactComponent as HeaderAvatarIcon } from '../../../images/icons/avatar.svg';
+import { ReactComponent as HeaderAvatarIcon } from '../../../images/icons/user-avatar.svg';
 import { ReactComponent as HelpCircleIcon } from '../../../images/icons/help-circle.svg';
 
 export const IconSVG = {
