7c229b81fde4576683372d582dc7906f4d8e0089
Nofar Baranes <nofar@reallygood.co.il>
2020-06-25 19:00:16 +0300

    WIP - Rendering range-picker when the user selects 'custom date'

--- src/components/common/filters/time-filter/index.tsx
+++ src/components/common/filters/time-filter/index.tsx
@@ -1,9 +1,12 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
 import cc from 'classcat';
 import { DownOutlined } from '@ant-design/icons';
-import { Menu, Dropdown, Button } from 'antd';
+import { Menu, Dropdown, Button, DatePicker } from 'antd';
+import moment from 'moment';
 
 import { ITimeFilterOptions, TimeOption } from '../filters.interface';
+
+import classes from './style.module.scss';
 
 const options: ITimeFilterOptions[] = [
   { id: TimeOption.Yesterday },
@@ -21,14 +24,26 @@ const options: ITimeFilterOptions[] = [
     },
   },
 ];
+const { RangePicker } = DatePicker;
 
 export default function TimeFilter({ className = '' }: { className?: string }): ReactElement {
   const [time, setTime] = useState(options[0]);
+
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  useEffect(() => {
+    setShowDatePicker(time.id === TimeOption.Custom);
+  }, [time]);
 
   const handleMenuClick = (e: any) => {
     setTime(options[e.key]);
     console.log('click', e);
   };
+
+  function onChange(dates: any, dateStrings: any) {
+    console.log('From: ', dates[0], ', to: ', dates[1]);
+    console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
+  }
 
   const menu = (
     <Menu onClick={handleMenuClick} className="wm-dropdown-menu time-filter">
@@ -41,14 +56,28 @@ export default function TimeFilter({ className = '' }: { className?: string }): 
   );
 
   return (
-    <div className={`wm-time-filter ${className}`}>
-      <span className="label">Time: </span>
-      <Dropdown overlay={menu}>
-        <Button type="link" className={`wm-btn filter-menu`}>
-          {time.id}
-          <DownOutlined />
-        </Button>
-      </Dropdown>
+    <div className={cc([classes['time-filter-container'], className])}>
+      <div className={classes['wm-time-filter']}>
+        <span className="label">Time: </span>
+        <Dropdown overlay={menu}>
+          <Button type="link" className={`wm-btn filter-menu`}>
+            {time.id}
+            <DownOutlined />
+          </Button>
+        </Dropdown>
+      </div>
+      {showDatePicker && (
+        <RangePicker
+          className={classes['wm-range-picker']}
+          bordered={false}
+          ranges={{
+            Today: [moment(), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+          }}
+          format="YYYY/MM/DD HH:mm:ss"
+          onChange={onChange}
+        />
+      )}
     </div>
   );
 }
