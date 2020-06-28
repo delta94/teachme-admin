1f1f6d33302aaaa6369f9a7567d1157810c4789a
Nofar Baranes <nofar@reallygood.co.il>
2020-06-28 19:34:48 +0300

    Replacing section html tag with fregment  from UsersScreen component

--- src/components/screen/UsersScreen/index.tsx
+++ src/components/screen/UsersScreen/index.tsx
@@ -5,16 +5,14 @@ import { usersMockData } from '../../../constants/mocks/users-mock';
 import { data as tableData, columns } from '../../../mocks/tableMockUsersData';
 import ScreenHeader from '../../common/ScreenHeader';
 
-import screenClasses from '../style.module.scss';
-
 export default function UsersScreen(): ReactElement {
   const { title: mainTitle, usersTable } = usersMockData;
   return (
-    <section className={screenClasses['screen']}>
+    <>
       <ScreenHeader title={mainTitle} />
       <WMCard title={`${tableData.length} ${usersTable.title}`}>
         <WMTable data={tableData} columns={columns} />
       </WMCard>
-    </section>
+    </>
   );
 }
