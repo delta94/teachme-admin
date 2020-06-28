5326b617af1a2581587dc0dfffb6f392958e9941
Dvir Avraham <dvir@reallygood.co.il>
2020-06-28 18:08:53 +0300

    Remove specific "type" prop and rename ts props type in `WMButton`

--- src/components/common/WMButton/index.tsx
+++ src/components/common/WMButton/index.tsx
@@ -2,16 +2,21 @@ import React, { ReactElement } from 'react';
 import { Button } from 'antd';
 import { ButtonProps } from 'antd/lib/button';
 
-export type PropTypes = {
+export type IWMButtonProps = {
   className?: string;
   icon?: ButtonProps['icon'];
   children?: React.ReactNode;
   props?: ButtonProps;
 };
 
-export default function WMButton({ className, icon, children, ...props }: PropTypes): ReactElement {
+export default function WMButton({
+  className,
+  icon,
+  children,
+  ...props
+}: IWMButtonProps): ReactElement {
   return (
-    <Button type="link" className={`wm-btn ${className}`} icon={icon} {...props}>
+    <Button className={`wm-btn ${className}`} icon={icon} {...props}>
       {children}
     </Button>
   );
