8d8aa97fb93b148dd5578a7d1e90b3a042c18100
Shiran Meir <shiran@reallygood.co.il>
2020-06-29 11:35:57 +0300

    Quiz results filter in users screen

--- src/components/Screen/UsersScreen/index.tsx
+++ src/components/Screen/UsersScreen/index.tsx
@@ -6,6 +6,7 @@ import { data as tableData, columns } from '../../../mocks/tableMockUsersData';
 import ScreenHeader from '../../common/ScreenHeader';
 import CoursesFilter from '../../common/filters/CoursesFilter';
 import StatusCourseFilter from '../../common/filters/StatusCourseFilter';
+import QuizResultsFilter from '../../common/filters/QuizResultsFilter';
 
 export default function UsersScreen(): ReactElement {
   const { title: mainTitle, usersTable } = usersMockData;
@@ -15,6 +16,7 @@ export default function UsersScreen(): ReactElement {
       <WMCard title={`${tableData.length} ${usersTable.title}`}>
         <CoursesFilter />
         <StatusCourseFilter />
+        <QuizResultsFilter />
         <WMTable data={tableData} columns={columns} />
       </WMCard>
     </>
