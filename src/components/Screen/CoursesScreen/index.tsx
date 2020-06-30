d109ae87190b771a56426d55eee0f57371d4761a
Denise Rosegg <denise@reallygood.co.il>
2020-06-30 22:33:41 +0300

    Moves `CoursesScreen` `PieBarChart`s into their own components

--- src/components/Screen/CoursesScreen/index.tsx
+++ src/components/Screen/CoursesScreen/index.tsx
@@ -2,17 +2,13 @@ import React, { ReactElement } from 'react';
 import cc from 'classcat';
 
 import { coursesMockData } from '../../../constants/mocks/courses-mock';
-import courseCompletionChartMock from '../../../mocks/courseCompletionChartMock';
-import courseCompletionRateChartMock from '../../../mocks/courseCompletionRateChartMock';
 import { data as tableData, columns } from '../../../mocks/tableMockData';
 
 import WMCard from '../../common/WMCard';
 import WMTable from '../../common/WMTable';
 import ScreenHeader from '../../common/ScreenHeader';
-import PieBarChart from '../../common/PieBarChart';
-import AvgCompletionTimeLegend from '../../common/AvgCompletionTimeLegend';
-import QuizCompletionRateLegend from '../../common/QuizCompletionRateLegend';
-import PieBarSummary from '../../common/PieBarSummary';
+import CoursesTimeCompletionChart from '../../common/CourseTimeCompletionChart';
+import QuizCompletionRateChart from '../../common/QuizCompletionRateChart';
 
 import classes from './style.module.scss';
 
@@ -32,27 +28,10 @@ export default function CoursesScreen(): ReactElement {
         </div>
         <div className={cc([classes.graphs, classes['right-graphs']])}>
           <WMCard title={graph_2.title}>
-            <PieBarSummary
-              value={courseCompletionChartMock.summaryLegend}
-              unit={` ${courseCompletionChartMock.summaryUnit}`}
-            />
-            <PieBarChart
-              bars={courseCompletionChartMock.bars}
-              totalValue={courseCompletionChartMock.totalValue}
-              legendContent={AvgCompletionTimeLegend}
-            />
+            <CoursesTimeCompletionChart />
           </WMCard>
           <WMCard title={graph_3.title}>
-            <PieBarSummary
-              value={courseCompletionRateChartMock.summaryLegend}
-              unit={courseCompletionRateChartMock.summaryUnit}
-              text={` (${courseCompletionRateChartMock.bars[0].value} of ${courseCompletionRateChartMock.totalValue} users)`}
-            />
-            <PieBarChart
-              bars={courseCompletionRateChartMock.bars}
-              totalValue={courseCompletionRateChartMock.totalValue}
-              legendContent={QuizCompletionRateLegend}
-            />
+            <QuizCompletionRateChart />
           </WMCard>
         </div>
       </div>
