dc06917716da12065fff120dc3bf3d6c61d649d9
Denise Rosegg <denise@reallygood.co.il>
2020-07-01 12:10:21 +0300

    Styles `WMTable`. Adds componentד for different cell types. Updates mock data.

--- src/components/common/WMTable/index.tsx
+++ src/components/common/WMTable/index.tsx
@@ -1,8 +1,12 @@
 import React, { ReactElement } from 'react';
 import { Table } from 'antd';
 
+import classes from './style.module.scss';
+
 type PropTypes = { columns?: any; data: Array<any> };
 
 export default function WMTable({ columns, data }: PropTypes): ReactElement {
-  return <Table columns={columns} dataSource={data} pagination={false} />;
+  return (
+    <Table className={classes['wm-table']} columns={columns} dataSource={data} pagination={false} />
+  );
 }
