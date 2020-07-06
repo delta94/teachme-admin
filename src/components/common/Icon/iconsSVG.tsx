852cba2f6c2cc57498d47d213ee5990ebe7fad25
Nofar Baranes <nofar@reallygood.co.il>
2020-07-06 09:56:07 +0300

    Fixing iconSVG imports/export

--- src/components/common/Icon/iconsSVG.tsx
+++ src/components/common/Icon/iconsSVG.tsx
@@ -3,7 +3,7 @@ import { ReactComponent as UsersIcon } from '../../../images/icons/sidebar-users
 import { ReactComponent as HeaderAvatarIcon } from '../../../images/icons/user-avatar.svg';
 import { ReactComponent as HelpCircleIcon } from '../../../images/icons/help-circle.svg';
 import { ReactComponent as FileExportIcon } from '../../../images/icons/file-export.svg';
-import { ReactComponent as SearchIcon } from '../../../images/icons/search.svg';
+// import { ReactComponent as SearchIcon } from '../../../images/icons/search.svg';
 import { ReactComponent as QuizIcon } from '../../../images/icons/quiz-tab.svg';
 
 import { IconType } from './icon.interface';
@@ -14,6 +14,6 @@ export const IconSVG = {
   [IconType.HeaderAvatar]: HeaderAvatarIcon,
   [IconType.HelpCircle]: HelpCircleIcon,
   [IconType.FileExport]: FileExportIcon,
-  [IconType.Search]: SearchIcon,
+  // [IconType.Search]: SearchIcon,
   [IconType.Quiz]: QuizIcon,
 };
