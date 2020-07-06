764c38bfb6d7e14857028caa67a17f0c275ba8f0
Shiran Meir <shiran@reallygood.co.il>
2020-07-06 10:47:25 +0300

    PR fixing

--- src/components/common/WMSwitch/index.tsx
+++ src/components/common/WMSwitch/index.tsx
@@ -2,18 +2,29 @@ import React, { ReactElement } from 'react';
 import { Switch } from 'antd';
 import classes from './style.module.scss';
 
-type PropTypes = { descriptionText: string; infoText?: string };
+export interface IWMSwitch {
+  label: string;
+  infoText?: string;
+}
 
-export default function WMSwitch({ descriptionText, infoText }: PropTypes): ReactElement {
+export default function IWMSwitch({ label, infoText, ...otherProps }: IWMSwitch): ReactElement {
   const onChange = (checked: any) => {
     console.log(`switch to ${checked}`);
   };
 
   return (
-    <div className={classes['switch-container']}>
-      <Switch size="small" className={classes['switch-btn']} defaultChecked onChange={onChange} />
-      <span className={classes['switch-description']}>{descriptionText}</span>
-      <div className={classes['switch-info']}>{infoText}</div>
+    <div className={classes['wm-switch']}>
+      <span className={classes['wm-switch-wrap']}>
+        <Switch
+          size="small"
+          className={classes['switch-btn']}
+          defaultChecked
+          onChange={onChange}
+          {...otherProps}
+        />
+        <span className={classes['switch-label']}>{label}</span>
+      </span>
+      <p className={classes['switch-info']}>{infoText}</p>
     </div>
   );
 }
