1d07315006f856dee97fcef3e89d2888ead1d4b9
Nofar Baranes <nofar@reallygood.co.il>
2020-07-06 14:02:59 +0300

    CR changes

--- src/components/Screen/CourseScreen/CourseTabs.tsx
+++ src/components/Screen/CourseScreen/CourseTabs.tsx
@@ -10,34 +10,50 @@ import CourseQuizTabCharts from '../../common/CourseQuizTabCharts';
 
 import WMCard from '../../common/WMCard';
 
+enum TabId {
+  Outline = 'outline',
+  Quiz = 'quiz',
+}
+
+const courseTabs = [
+  {
+    id: TabId.Outline,
+    title: 'Outline',
+    itemsLength: 16,
+    icon: <Icon type={IconType.SidebarCourses} />,
+    content: <CourseQuizTabCharts data={quizBarChartMock} />,
+  },
+  {
+    id: TabId.Quiz,
+    title: 'Quiz',
+    itemsLength: quizBarChartMock.questions.length,
+    icon: <Icon type={IconType.Quiz} />,
+    content: 'Tab 1 content',
+  },
+];
+
 export default function CourseTabs({ course }: { course: any }): ReactElement {
   return (
     <WMCard>
-      <WMTabs defaultActiveKey="outline">
-        <WMTabPanel
-          tab={
-            <>
-              <Icon type={IconType.SidebarCourses} />
-              <span>Outline (16)</span>
-            </>
-          }
-          key="outline"
-        >
-          Tab 1 content
-        </WMTabPanel>
-        <WMTabPanel
-          tab={
-            <>
-              <Icon type={IconType.Quiz} />
-              <span>
-                Quiz {quizBarChartMock.questions.length && `(${quizBarChartMock.questions.length})`}
-              </span>
-            </>
-          }
-          key="quiz"
-        >
-          <CourseQuizTabCharts data={quizBarChartMock} />
-        </WMTabPanel>
+      <WMTabs defaultActiveKey={TabId.Outline}>
+        {courseTabs.map((tab) => {
+          const { id, title, itemsLength, icon, content } = tab;
+          return (
+            <WMTabPanel
+              tab={
+                <>
+                  {icon}
+                  <span>
+                    {title} {itemsLength && `(${itemsLength})`}
+                  </span>
+                </>
+              }
+              key={id}
+            >
+              {content}
+            </WMTabPanel>
+          );
+        })}
       </WMTabs>
     </WMCard>
   );
