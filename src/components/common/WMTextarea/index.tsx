5061f56cd24a335bb2a1f0346485a97d1517cfce
Denise Rosegg <denise@reallygood.co.il>
2020-07-08 21:32:40 +0300

    Adds `WMTextarea` component.

---
+++ src/components/common/WMTextarea/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import cc from 'classcat';
+import { Input } from 'antd';
+import { TextAreaProps } from 'antd/lib/input';
+import { AutoSizeType } from 'antd/lib/input/ResizableTextArea';
+
+import classes from './style.module.scss';
+const { TextArea } = Input;
+
+export interface IWMInputProps extends TextAreaProps {
+  className?: string;
+  autosize?: boolean | AutoSizeType;
+}
+
+export default function WMTextarea({
+  className,
+  autosize = { minRows: 3, maxRows: 5 },
+  ...otherProps
+}: IWMInputProps) {
+  return (
+    <TextArea
+      className={cc([classes['wm-textarea'], className])}
+      autoSize={autosize}
+      {...otherProps}
+    />
+  );
+}
