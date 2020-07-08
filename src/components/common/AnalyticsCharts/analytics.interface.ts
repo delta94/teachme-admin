17fc5c3483883e28d251f6970dc5ee1aff8ff5bf
Nofar Baranes <nofar@reallygood.co.il>
2020-07-08 17:36:28 +0300

    adding styles and types to analytics.interface

--- src/components/common/AnalyticsCharts/analytics.interface.ts
+++ src/components/common/AnalyticsCharts/analytics.interface.ts
@@ -1,21 +1,21 @@
-// TODO: create a properly interface instead of using any
 export interface IAnalyticsCharts {
   data: any;
 }
 
-// TODO: create a properly interface instead of using any
 export interface ICourseStatusChart {
   summaryData: any;
 }
 
-// TODO: create a properly interface instead of using any
 export interface ICoursesTimeCompletionChart {
   completionData: any;
 }
 
-// TODO: create a properly interface instead of using any
 export interface IQuizCompletionRateChart {
   quizCompletionData: any;
+}
+
+export interface IQuizScoreData {
+  quizScoreData: any;
 }
 
 export interface ICourseByDay {
