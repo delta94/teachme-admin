f109e410d2a8f43bf7607000ffd8bba53e0acd8f
Denise Rosegg <denise@reallygood.co.il>
2020-07-08 11:09:33 +0300

    Fixes PR comments.

--- src/components/common/dialogs/PublishToEnvironmentDialog/index.tsx
+++ src/components/common/dialogs/PublishToEnvironmentDialog/index.tsx
@@ -20,34 +20,32 @@ export default function PublishToEnvironmentDialog({
   const [environment, setEnvironment] = useState<IWMDropdownOption>(environments[0]);
 
   return (
-    <>
-      <WMConfirmationDialog
-        open={open}
-        title={
-          <div className={classes['publish-dialog-title']}>
-            <div>Publish to </div>
-            <EnvironmentDropdown
-              environments={environments}
-              intialSelectedEnvironment={environment}
-              onChange={(selected) => setEnvironment(selected)}
-            />
-          </div>
-        }
-        confirmLabel={`Publish to ${environment.text}`}
-        onCancel={onCancel}
-        onConfirm={onConfirm}
-      >
-        <p>You are about to publish the following courses. Please review before confirmation.</p>
-        <ul className={classes['publish-dialog-ul']}>
-          <li>
-            <VIcon />1 course
-          </li>
-          <li>
-            <VIcon />
-            Global settings
-          </li>
-        </ul>
-      </WMConfirmationDialog>
-    </>
+    <WMConfirmationDialog
+      open={open}
+      title={
+        <div className={classes['publish-dialog-title']}>
+          <div>Publish to </div>
+          <EnvironmentDropdown
+            environments={environments}
+            intialSelectedEnvironment={environment}
+            onChange={(selected) => setEnvironment(selected)}
+          />
+        </div>
+      }
+      confirmLabel={`Publish to ${environment.text}`}
+      onCancel={onCancel}
+      onConfirm={onConfirm}
+    >
+      <p>You are about to publish the following courses. Please review before confirmation.</p>
+      <ul className={classes['publish-dialog-ul']}>
+        <li>
+          <VIcon />1 course
+        </li>
+        <li>
+          <VIcon />
+          Global settings
+        </li>
+      </ul>
+    </WMConfirmationDialog>
   );
 }
