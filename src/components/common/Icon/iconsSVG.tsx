d5895351e9f258af0a31c08c0f5d9d625c2d53df
Dvir Avraham <dvir@reallygood.co.il>
2020-07-09 13:34:35 +0300

    Fix icon imports

--- src/components/common/Icon/iconsSVG.tsx
+++ src/components/common/Icon/iconsSVG.tsx
@@ -11,7 +11,7 @@ import { ReactComponent as SmartWalkthruSmallIcon } from '../../../images/icons/
 import { ReactComponent as VideoIcon } from '../../../images/icons/video.svg';
 import { ReactComponent as VideoSmallIcon } from '../../../images/icons/video-small.svg';
 import { ReactComponent as ArticleIcon } from '../../../images/icons/article.svg';
-import { ReactComponent as ArticleIconSmall } from '../../../images/icons/article-small.svg';
+import { ReactComponent as ArticleSmallIcon } from '../../../images/icons/article-small.svg';
 import { ReactComponent as RefreshIcon } from '../../../images/icons/refresh.svg';
 
 import { IconType } from './icon.interface';
@@ -28,9 +28,9 @@ export const IconSVG = {
   [IconType.SmartWalkthru]: SmartWalkthruIcon,
   [IconType.SmartWalkthruSmall]: SmartWalkthruSmallIcon,
   [IconType.Article]: ArticleIcon,
-  [IconType.ArticleSmall]: VideoSmallIcon,
+  [IconType.ArticleSmall]: ArticleSmallIcon,
   [IconType.Video]: VideoIcon,
-  [IconType.VideoSmall]: ArticleIconSmall,
+  [IconType.VideoSmall]: VideoSmallIcon,
   [IconType.Content]: VideoIcon, // TODO: should remove after fixing course item type
   [IconType.Refresh]: RefreshIcon,
 };
