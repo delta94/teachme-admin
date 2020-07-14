16970b804ca1cb28b06d4ffa3551ef9049376d89
eli.b <eli.b@walkme.com>
2020-07-14 18:41:55 +0300

    fix get course

--- src/walkme/segments.ts
+++ src/walkme/segments.ts
@@ -3,20 +3,20 @@ import { getData } from './data';
 
 export async function getCourseSegments(
   course_id: number,
-  environment_id: number
+  environment_id: number,
 ): Promise<Array<string>> {
   const segments = (await getData(TypeName.Tag, environment_id)) as Array<WalkMeDataTag>;
   return segments
-    .filter(segment => isInSegment(segment, TypeName.Course, course_id))
-    .map(segment => segment.Name);
+    .filter((segment) => isInSegment(segment, TypeName.Course, course_id))
+    .map((segment) => segment.Name);
 }
 
 function isInSegment(segment: WalkMeDataTag, type: TypeName, id: number): boolean {
   return segment.LinkedDeployables.filter(getTypeFilter(type))
-    .map(item => item.DeployableID)
+    .map((item) => item.DeployableID)
     .includes(id);
 }
 
 const getTypeFilter = (type: TypeName) =>
   //@ts-ignore
-  (item: WalkMeLink) => item.DeployableType == parseInt(TypeId[type]);
+  (item: WalkMeNewLink) => item.DeployableType == parseInt(TypeId[type]);
