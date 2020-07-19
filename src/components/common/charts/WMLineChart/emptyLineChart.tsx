fa1bb7115c50add1e42a9adaf5420354f6565aa7
Shiran Meir <shiran@reallygood.co.il>
2020-07-19 12:07:33 +0300

    Changing mockdata and responsive container

--- src/components/common/charts/WMLineChart/emptyLineChart.tsx
+++ src/components/common/charts/WMLineChart/emptyLineChart.tsx
@@ -1,23 +1,40 @@
 import React, { ReactElement } from 'react';
-import { LineChart, XAxis, YAxis, CartesianGrid, Line } from 'recharts';
+import { LineChart, XAxis, YAxis, CartesianGrid, Line, ResponsiveContainer } from 'recharts';
 
 import emptyChartMockData from './emptyChartMock';
+
+import classes from './style.module.scss';
 
 export default function EmptyLineChart({ className = '' }: { className: string }): ReactElement {
   return (
     <div className={className}>
-      <LineChart
-        width={730}
-        height={250}
-        data={emptyChartMockData}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Line type="monotone" dataKey="pv" dot={false} stroke="#c1c1c1" isAnimationActive={false} />
-        <Line type="monotone" dataKey="uv" dot={false} stroke="#c1c1c1" isAnimationActive={false} />
-      </LineChart>
+      <ResponsiveContainer width="100%" height={250}>
+        <LineChart
+          className={classes['course-summary-chart']}
+          width={730}
+          height={250}
+          data={emptyChartMockData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            dot={false}
+            stroke="#c1c1c1"
+            isAnimationActive={false}
+          />
+          <Line
+            type="monotone"
+            dataKey="uv"
+            dot={false}
+            stroke="#c1c1c1"
+            isAnimationActive={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 }
