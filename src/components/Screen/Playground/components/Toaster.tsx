9920ab6dbedf65c96e07c2ab2e831ea78ddf26e6
Shiran Meir <shiran@reallygood.co.il>
2020-07-21 12:14:03 +0300

    Added switch case to toaster

--- src/components/Screen/Playground/components/Toaster.tsx
+++ src/components/Screen/Playground/components/Toaster.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement } from 'react';
 
 import { Button, Space } from 'antd';
-import { wmMessageSuccess } from '../../../common/wmMessage';
+import { wmMessage } from '../../../common/wmMessage';
 
 export default function Toaster(): ReactElement {
   return (
     <Space>
-      <Button onClick={() => wmMessageSuccess('1 course published to production')}>Button</Button>
+      <Button onClick={() => wmMessage('1 course published to production')}>Button</Button>
     </Space>
   );
 }
