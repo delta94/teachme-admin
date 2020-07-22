67e021c3d74fcf0e1e7f3a03b7c92858442fe168
Nofar Baranes <nofar@reallygood.co.il>
2020-07-22 17:11:44 +0300

    Fixing QuizSettingsForm data flow

--- src/components/Screen/Playground/components/QuizSettings.tsx
+++ src/components/Screen/Playground/components/QuizSettings.tsx
@@ -2,6 +2,7 @@ import React, { ReactElement, useState } from 'react';
 import { Divider } from 'antd';
 import cc from 'classcat';
 
+import CourseEditorProvider from '../../../../providers/CourseEditorContext';
 import WMButton, { ButtonVariantEnum } from '../../../common/WMButton';
 import WMCard from '../../../common/WMCard';
 import DetailsPanel from '../../../common/DetailsPanel';
@@ -14,29 +15,31 @@ export default function QuizSettings(): ReactElement {
   const [courseId, setCourseId] = useState(0);
 
   return (
-    <div className={classes['cards-wrapper']}>
-      <WMCard className={cc([classes['buttons'], classes['grow']])}>
-        <WMButton variant={ButtonVariantEnum.Primary} onClick={() => setCourseId(1284870)}>
-          Quiz Settings - courseId 1284870
-        </WMButton>
-        <Divider />
-        <WMButton variant={ButtonVariantEnum.Primary} onClick={() => setCourseId(1297234)}>
-          Quiz Settings - courseId 1297234
-        </WMButton>
-        <Divider />
-        <WMButton variant={ButtonVariantEnum.Primary} onClick={() => setCourseId(1277328)}>
-          Quiz Settings - courseId 1277328
-        </WMButton>
-        <Divider />
-      </WMCard>
-      <DetailsPanel
-        title="Quiz Settings"
-        titleIcon={<Icon type={IconType.QuizSettings} />}
-        isOpen={Boolean(courseId)}
-        onClose={() => setCourseId(0)}
-      >
-        <QuizSettingsForm courseId={courseId} />
-      </DetailsPanel>
-    </div>
+    <CourseEditorProvider>
+      <div className={classes['cards-wrapper']}>
+        <WMCard className={cc([classes['buttons'], classes['grow']])}>
+          <WMButton variant={ButtonVariantEnum.Primary} onClick={() => setCourseId(1284870)}>
+            Quiz Settings - courseId 1284870
+          </WMButton>
+          <Divider />
+          <WMButton variant={ButtonVariantEnum.Primary} onClick={() => setCourseId(1297234)}>
+            Quiz Settings - courseId 1297234
+          </WMButton>
+          <Divider />
+          <WMButton variant={ButtonVariantEnum.Primary} onClick={() => setCourseId(1277328)}>
+            Quiz Settings - courseId 1277328
+          </WMButton>
+          <Divider />
+        </WMCard>
+        <DetailsPanel
+          title="Quiz Settings"
+          titleIcon={<Icon type={IconType.QuizSettings} />}
+          isOpen={Boolean(courseId)}
+          onClose={() => setCourseId(0)}
+        >
+          <QuizSettingsForm courseId={courseId} />
+        </DetailsPanel>
+      </div>
+    </CourseEditorProvider>
   );
 }
