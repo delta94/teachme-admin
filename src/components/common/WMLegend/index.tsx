c83cc812d890023a2c489794e61269c3bc669553
Shiran Meir <shiran@reallygood.co.il>
2020-07-23 16:23:49 +0300

    WMLegend skeleton

--- src/components/common/WMLegend/index.tsx
+++ src/components/common/WMLegend/index.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, ReactElement } from 'react';
+import React, { ReactNode, ReactElement, useState, useEffect } from 'react';
 import cc from 'classcat';
 
+import { useAppContext } from '../../../providers/AppContext';
 import Header from '../Header';
 import StatusDot, { DotType } from '../StatusDot';
+import WMSkeleton from '../WMSkeleton';
 import EmptyLegend from './EmptyLegend';
 
 import classes from './style.module.scss';
@@ -22,22 +24,36 @@ export default function WMLegend({
   children,
   hasData,
 }: IWMLegend): ReactElement {
+  const [appState, appDispatch] = useAppContext();
+  const { isUpdating } = appState;
+  const [appInit, setAppInit] = useState(false);
+
+  useEffect(() => {
+    if (!isUpdating && !appInit) setAppInit(true);
+  }, [isUpdating, appInit]);
+
   return (
     <>
-      {hasData ? (
-        <div className={cc([classes['legend'], className])}>
-          <Header className={classes['legend-header']}>
-            <StatusDot
-              className={classes['legend-dot-status']}
-              type={DotType.Custom}
-              dotColor={dotStatusColor}
-            />
-            <span className={classes['legend-title']}>{title}</span>
-          </Header>
-          {children}
-        </div>
+      {appInit ? (
+        <>
+          {hasData ? (
+            <div className={cc([classes['legend'], className])}>
+              <Header className={classes['legend-header']}>
+                <StatusDot
+                  className={classes['legend-dot-status']}
+                  type={DotType.Custom}
+                  dotColor={dotStatusColor}
+                />
+                <span className={classes['legend-title']}>{title}</span>
+              </Header>
+              {children}
+            </div>
+          ) : (
+            <EmptyLegend title={title} />
+          )}
+        </>
       ) : (
-        <EmptyLegend title={title} />
+        <WMSkeleton active paragraph={{ rows: 2 }} />
       )}
     </>
   );
