8bfd635c1cca6bb7c64bb9a104ade0ad97e1ad09
Shiran Meir <shiran@reallygood.co.il>
2020-07-27 14:07:03 +0300

    Outline and Quiz course skeleton

--- src/components/Screen/CourseScreen/CourseTabs.tsx
+++ src/components/Screen/CourseScreen/CourseTabs.tsx
@@ -1,7 +1,6 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement } from 'react';
 
 import { courseMockData } from '../../../constants/mocks/course-screen';
-import { useAppContext } from '../../../providers/AppContext';
 
 import WMTabs from '../../common/WMTabs';
 import WMTabPanel from '../../common/WMTabs/WMTabPanel';
@@ -9,7 +8,6 @@ import Icon from '../../common/Icon';
 import { IconType } from '../../common/Icon/icon.interface';
 import CourseQuizTabCharts from '../../common/CourseQuizTabCharts';
 import WMCard from '../../common/WMCard';
-import WMSkeleton from '../../common/WMSkeleton';
 import CourseOutlineTable from './CourseOutlineTable';
 import { ICourseTabs } from './courseScreen.interface';
 
@@ -37,14 +35,6 @@ export default function CourseTabs({ course }: ICourseTabs): ReactElement {
     },
   ];
 
-  const [appState, appDispatch] = useAppContext();
-  const { isUpdating } = appState;
-  const [appInit, setAppInit] = useState(false);
-
-  useEffect(() => {
-    if (!isUpdating && !appInit) setAppInit(true);
-  }, [isUpdating, appInit]);
-
   return (
     <WMCard>
       <WMTabs defaultActiveKey={TabId.Outline}>
@@ -52,25 +42,19 @@ export default function CourseTabs({ course }: ICourseTabs): ReactElement {
           const { id, title, itemsLength, icon, content } = tab;
 
           return (
-            <>
-              {appInit ? (
-                <WMTabPanel
-                  tab={
-                    <>
-                      {icon}
-                      <span>
-                        {title} {itemsLength && `(${itemsLength})`}
-                      </span>
-                    </>
-                  }
-                  key={id}
-                >
-                  {content}
-                </WMTabPanel>
-              ) : (
-                <WMSkeleton active paragraph={{ rows: 10 }} />
-              )}
-            </>
+            <WMTabPanel
+              tab={
+                <>
+                  {icon}
+                  <span>
+                    {title} {itemsLength && `(${itemsLength})`}
+                  </span>
+                </>
+              }
+              key={id}
+            >
+              {content}
+            </WMTabPanel>
           );
         })}
       </WMTabs>
