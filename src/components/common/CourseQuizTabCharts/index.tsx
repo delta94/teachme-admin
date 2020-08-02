e3f2cfa3093c2574e0ba03136cc4071af612ea71
Nofar Baranes <nofar@reallygood.co.il>
2020-08-02 20:25:17 +0300

    Fixing CourseQuizTabCharts data

--- src/components/common/CourseQuizTabCharts/index.tsx
+++ src/components/common/CourseQuizTabCharts/index.tsx
@@ -8,32 +8,10 @@ import { WMHorizontalBarChart } from '../charts';
 
 import classes from './style.module.scss';
 
+const reduceTotalResponses = (accumulator: any, { total_responses }: { total_responses: number }) =>
+  accumulator + total_responses;
+
 export default function CourseQuizTabCharts({ quiz }: { quiz: QuizOutlineUI }): ReactElement {
-  /**
-   * The data doesn't match to the expected data structure.
-   * using the commented code and the console.log as reference.
-   * TODO: remove the comments and the hard-coded values after getting a proper data
-   * */
-
-  // console.log('quiz ', quiz);
-
-  // return (
-  //   <div className={classes['course-quiz-tab-charts']}>
-  //     {data.questions.map((question: IQuizQuestions, index: number) => {
-  //       const isLast = index === data.questions.length - 1;
-
-  //       return (
-  //         <div key={question.title}>
-  //           <div className={classes['question-title']}>{question.title}</div>
-  //           <div className={classes['question-type']}>{question.type}</div>
-  //           <WMHorizontalBarChart bars={question.answers} totalValue={question.totalValue} />
-  //           {!isLast && <Divider className={classes['question-divider']} />}
-  //         </div>
-  //       );
-  //     })}
-  //   </div>
-  // );
-
   return (
     <div className={classes['course-quiz-tab-charts']}>
       {quiz.questions.map((question: QuizOutlineUIQuestion, index: number) => {
@@ -46,12 +24,12 @@ export default function CourseQuizTabCharts({ quiz }: { quiz: QuizOutlineUI }): 
               QuestionType[question.type]
             } Select`}</div>
             <WMHorizontalBarChart
-              bars={question.answers.map(({ title, isCorrect }) => ({
+              bars={question.answers.map(({ title, isCorrect, countAnswers }) => ({
                 title,
                 isCorrect,
-                value: 0, // missing data
+                value: countAnswers,
               }))}
-              totalValue={0} // missing data
+              totalValue={quiz.questions.reduce(reduceTotalResponses, 0)}
             />
             {!isLast && <Divider className={classes['question-divider']} />}
           </div>
