2af7b69ecdd1abe9e44c884a10d1e11eab485f03
Denise Rosegg <denise@reallygood.co.il>
2020-08-04 13:17:03 +0300

    Adds buffer so that scrolling looks nicer

--- src/components/Screen/CourseEditorScreen/ResourceItemList/index.tsx
+++ src/components/Screen/CourseEditorScreen/ResourceItemList/index.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import { Container } from 'react-smooth-dnd';
 import { ContentItem } from '@walkme/types';
-import cc from 'classcat';
 
 import TaskItem from '../TaskItem';
 
@@ -17,19 +16,29 @@ export interface ICourseItemsList {
 const getVirtualizationProps = (
   itemAmount: number,
   itemHeight: number,
+  itemBuffer: number,
   containerRef?: HTMLDivElement | null,
 ) => {
+  const lastItemIndex = itemAmount - 1;
   const scrollTop = containerRef?.scrollTop ?? 0;
   const containerHeight = containerRef?.getBoundingClientRect().height ?? 0;
 
+  // before
   const firstVisibleElement = Math.floor(scrollTop / itemHeight);
-  const lastVisibleElement = firstVisibleElement + Math.ceil(containerHeight / itemHeight);
-  const beforeFillerHeight = itemHeight * Math.max(0, firstVisibleElement - 1);
-  const afterFillerHeight = itemHeight * (itemAmount - lastVisibleElement);
+  const firstVisibleElementWithBuffer = Math.max(0, firstVisibleElement - itemBuffer);
+  const beforeFillerHeight = itemHeight * Math.max(0, firstVisibleElementWithBuffer - 1);
+
+  // after
+  const lastVisibleElement = Math.min(
+    firstVisibleElement + Math.ceil(containerHeight / itemHeight),
+    lastItemIndex,
+  );
+  const lastVisibleElementWithBuffer = Math.min(lastVisibleElement + itemBuffer, lastItemIndex);
+  const afterFillerHeight = itemHeight * (itemAmount - lastVisibleElementWithBuffer - 1);
 
   return {
-    firstVisibleElement,
-    lastVisibleElement,
+    firstRenderedElement: firstVisibleElementWithBuffer,
+    lastRenderedElement: lastVisibleElementWithBuffer,
     beforeFillerHeight,
     afterFillerHeight,
   };
@@ -40,6 +49,9 @@ const getRangeArray = (from: number, to: number) => {
   return Array.from({ length }, (v, k) => from + k);
 };
 
+const itemHeight = 35;
+const itemBuffer = 16;
+
 export default function ResourceItemsList({
   items,
   className,
@@ -47,34 +59,33 @@ export default function ResourceItemsList({
   isDisabled,
   ...otherProps
 }: ICourseItemsList): ReactElement {
-  const itemHeight = 35;
   const containerRef = useRef<HTMLDivElement>(null);
   const [virtualizationProps, setVirtualizationProps] = useState(
-    getVirtualizationProps(items.length, itemHeight),
+    getVirtualizationProps(items.length, itemHeight, itemBuffer, null),
   );
 
   const onContainerScroll = () => {
-    setVirtualizationProps(getVirtualizationProps(items.length, itemHeight, containerRef.current));
+    if (containerRef.current) {
+      setVirtualizationProps(
+        getVirtualizationProps(items.length, itemHeight, itemBuffer, containerRef.current),
+      );
+    }
   };
 
   const getIsVisible = (index: number) =>
-    index >= virtualizationProps.firstVisibleElement &&
-    index <= virtualizationProps.lastVisibleElement;
+    index >= virtualizationProps.firstRenderedElement &&
+    index <= virtualizationProps.lastRenderedElement;
 
   useEffect(() => {
     if (containerRef.current) {
       setVirtualizationProps(
-        getVirtualizationProps(items.length, itemHeight, containerRef.current),
+        getVirtualizationProps(items.length, itemHeight, itemBuffer, containerRef.current),
       );
     }
   }, [containerRef.current, items.length]);
 
   return (
-    <div
-      className={cc([classes['resource-items-list'], className])}
-      ref={containerRef}
-      onScroll={onContainerScroll}
-    >
+    <div className={className} ref={containerRef} onScroll={onContainerScroll}>
       <div style={{ height: virtualizationProps.beforeFillerHeight }} />
       <Container
         {...otherProps}
@@ -83,8 +94,8 @@ export default function ResourceItemsList({
           // getChildPayload uses an internal index that reflects the rendered element's index in the DOM
           // therefore we must map the index to the index in the items array using the indexes we know we rendered
           // so that we may access the correct item data
-          const { firstVisibleElement, lastVisibleElement } = virtualizationProps;
-          const indexMap = getRangeArray(firstVisibleElement, lastVisibleElement);
+          const { firstRenderedElement, lastRenderedElement } = virtualizationProps;
+          const indexMap = getRangeArray(firstRenderedElement, lastRenderedElement);
 
           return items[indexMap[i]];
         }}
