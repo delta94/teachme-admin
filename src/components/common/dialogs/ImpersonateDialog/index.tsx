b8b8cfc65bb767ea260cd108f6ff1b9ab2eae489
Shiran Meir <shiran@reallygood.co.il>
2020-08-04 08:27:20 +0300

    Added auto-compplete and impersonate logic to dialog

--- src/components/common/dialogs/ImpersonateDialog/index.tsx
+++ src/components/common/dialogs/ImpersonateDialog/index.tsx
@@ -1,7 +1,12 @@
 import React, { ReactElement, useState } from 'react';
 
+import { AutoComplete } from 'antd';
+
+import { getEmails } from '../../../../walkme';
+import { impersonate } from '../../../../walkme';
 import WMConfirmationDialog, { IWMConfirmationDialogWrapper } from '../../WMConfirmationDialog';
-import WMInput from '../../WMInput';
+
+import classes from './style.module.scss';
 
 export interface IImpersonateDialog extends IWMConfirmationDialogWrapper {
   value?: string;
@@ -14,19 +19,37 @@ export default function ImpersonateDialog({
   value,
 }: IImpersonateDialog): ReactElement {
   const [localValue, setLocalValue] = useState(value);
+  const [options, setOptions] = useState<{ value: string }[]>([]);
 
-  function onEmailChange(e: any) {
-    setLocalValue(e.target.value);
+  async function onEmailChange(value: string) {
+    const { emails } = await getEmails(value);
+    const emailList = emails.map((email) => ({ value: email }));
+    setOptions(emailList);
+    setLocalValue(value);
   }
+
+  const onConfirmValue = (userValue: string | undefined) => {
+    onConfirm(userValue);
+    userValue && impersonate(userValue);
+  };
 
   return (
     <WMConfirmationDialog
       open={open}
       title="Impersonate user"
       onCancel={onCancel}
-      onConfirm={() => onConfirm(localValue)}
+      onConfirm={() => onConfirmValue(localValue)}
     >
-      <WMInput placeholder="Enter email address" onChange={onEmailChange} value={localValue} />
+      <AutoComplete
+        className={classes['impersonate-dialog']}
+        options={options}
+        onChange={onEmailChange}
+        value={localValue}
+        placeholder="Enter email address"
+        filterOption={(inputValue, option) =>
+          option?.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+        }
+      />
     </WMConfirmationDialog>
   );
 }
