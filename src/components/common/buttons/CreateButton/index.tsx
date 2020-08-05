cc973d37d736cd545c1f66674b8bfb66d855b50a
Dvir Avraham <dvir@reallygood.co.il>
2020-08-05 18:16:29 +0300

    Fix create button

--- src/components/common/buttons/CreateButton/index.tsx
+++ src/components/common/buttons/CreateButton/index.tsx
@@ -1,16 +1,21 @@
 import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+
+import { NEW_COURSE_EDITOR_ROUTE } from '../../../../constants/routes';
 
 import Icon, { IconType } from '../../Icon';
 import WMButton, { ButtonVariantEnum } from '../../WMButton';
 
 export default function CreateButton({ className }: { className?: string }): ReactElement {
   return (
-    <WMButton
-      className={className}
-      type="link"
-      href="/course-editor/new"
-      variant={ButtonVariantEnum.Create}
-      icon={<Icon type={IconType.Plus} />}
-    />
+    <Link to={NEW_COURSE_EDITOR_ROUTE.path}>
+      <WMButton
+        className={className}
+        type="link"
+        href="/course-editor/new"
+        variant={ButtonVariantEnum.Create}
+        icon={<Icon type={IconType.Plus} />}
+      />
+    </Link>
   );
 }
