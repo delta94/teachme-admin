ed87e6bd12605db476b8606291575590b166a765
Denise Rosegg <denise@reallygood.co.il>
2020-08-09 14:28:40 +0300

    Removes the export dialog until exporting to e-mail is available. Disables actions when loading and when there are no results to export.

--- src/components/Screen/CourseScreen/ExportCourseButton.tsx
+++ src/components/Screen/CourseScreen/ExportCourseButton.tsx
@@ -1,27 +1,31 @@
-import React, { ReactElement, useState } from 'react';
+// todo: ExportToCSVDialog is commented out until export to email is implemented
+
+import React, { ReactElement /* , useState */ } from 'react';
 
 import { useCourseContext, exportCourse } from '../../../providers/CourseContext';
 
-import { ExportToCSVDialog } from '../../common/dialogs';
+/* import { ExportToCSVDialog } from '../../common/dialogs'; */
 import { ExportButton } from '../../common/buttons';
 
 import classes from './style.module.scss';
 
-export default function ExportCoursesButton(): ReactElement {
+export default function ExportCoursesButton({ disabled }: { disabled?: boolean }): ReactElement {
   const [state, dispatch] = useCourseContext();
   const {
     courseMetadata,
     dateRange: { from, to },
   } = state;
 
-  const [showExport, setShowExport] = useState(false);
+  /* const [showExport, setShowExport] = useState(false); */
 
   return (
     <>
-      {courseMetadata?.id && (
-        <>
-          <ExportButton className={classes['export']} onClick={() => setShowExport(true)} />
-          <ExportToCSVDialog
+      <ExportButton
+        className={classes['export']}
+        onClick={() => exportCourse(dispatch, courseMetadata?.id ?? 0, 0, from, to)}
+        disabled={disabled}
+      />
+      {/* <ExportToCSVDialog
             coursesCount={1}
             open={showExport}
             onCancel={() => setShowExport(false)}
@@ -29,9 +33,7 @@ export default function ExportCoursesButton(): ReactElement {
               setShowExport(false);
               exportCourse(dispatch, courseMetadata?.id, 0, from, to);
             }}
-          />
-        </>
-      )}
+          /> */}
     </>
   );
 }
