d77ab71ee448a596bd707a44b6301266f2b9b32f
Nofar Baranes <nofar@reallygood.co.il>
2020-08-17 17:51:08 +0300

    Cleanups

--- src/components/Screen/CourseScreen/CourseQuizEmptyState.tsx
+++ src/components/Screen/CourseScreen/CourseQuizEmptyState.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React, { ReactElement } from 'react';
 import cc from 'classcat';
 
 import Icon, { IconType } from '../../common/Icon';
