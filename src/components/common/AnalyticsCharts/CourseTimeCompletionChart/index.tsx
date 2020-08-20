333c9e437d41ddeba7f6cf9f480c8c544ed0f6af
Denise Rosegg <denise@reallygood.co.il>
2020-08-20 10:31:12 +0300

    Adds `React.memo()` to charts

--- src/components/common/AnalyticsCharts/CourseTimeCompletionChart/index.tsx
+++ src/components/common/AnalyticsCharts/CourseTimeCompletionChart/index.tsx
@@ -10,45 +10,51 @@ import { parseBucketsToPieBarSummary } from '../utils';
 
 import AvgCompletionTimeLegend from './CourseTimeCompletionLegend';
 
-export default function CoursesTimeCompletionChart({
-  className,
-  title,
-  overview,
-  isLoading = false,
-}: ICoursesTimeCompletionChart): ReactElement {
-  const [completionTimeAvg, setCompletionTimeAvg] = useState<number>();
-  const [bars, setBars] = useState<IBar[]>([]);
+const CoursesTimeCompletionChart = React.memo(
+  ({
+    className,
+    title,
+    overview,
+    isLoading = false,
+  }: ICoursesTimeCompletionChart): ReactElement => {
+    const [completionTimeAvg, setCompletionTimeAvg] = useState<number>();
+    const [bars, setBars] = useState<IBar[]>([]);
 
-  useEffect(() => {
-    if (overview?.completion_time) {
-      const { completion_time } = overview;
+    useEffect(() => {
+      if (overview?.completion_time) {
+        const { completion_time } = overview;
 
-      setCompletionTimeAvg(
-        completion_time.avg ? parseInt(completion_time.avg.toFixed(0)) : undefined,
-      );
+        setCompletionTimeAvg(
+          completion_time.avg ? parseInt(completion_time.avg.toFixed(0)) : undefined,
+        );
 
-      if (completion_time.buckets.length)
-        setBars(parseBucketsToPieBarSummary(completion_time.buckets));
-    }
-  }, [overview]);
+        if (completion_time.buckets.length)
+          setBars(parseBucketsToPieBarSummary(completion_time.buckets));
+      }
+    }, [overview]);
 
-  // unmount only
-  useEffect(
-    () => () => {
-      setCompletionTimeAvg(0);
-      setBars([]);
-    },
-    [],
-  );
+    // unmount only
+    useEffect(
+      () => () => {
+        setCompletionTimeAvg(0);
+        setBars([]);
+      },
+      [],
+    );
 
-  return (
-    <WMCard title={title}>
-      <WMSkeleton loading={isLoading} active paragraph={{ rows: 2 }}>
-        <div className={className}>
-          <PieBarSummary value={completionTimeAvg} unit=" hours" />
-          <PieBarChart bars={bars} legendContent={AvgCompletionTimeLegend} />
-        </div>
-      </WMSkeleton>
-    </WMCard>
-  );
-}
+    return (
+      <WMCard title={title}>
+        <WMSkeleton loading={isLoading} active paragraph={{ rows: 2 }}>
+          <div className={className}>
+            <PieBarSummary value={completionTimeAvg} unit=" hours" />
+            <PieBarChart bars={bars} legendContent={AvgCompletionTimeLegend} />
+          </div>
+        </WMSkeleton>
+      </WMCard>
+    );
+  },
+);
+
+CoursesTimeCompletionChart.displayName = 'CoursesTimeCompletionChart';
+
+export default CoursesTimeCompletionChart;
