88b52c69f39698dcce76de01038526fec39980c1
Denise Rosegg <denise@reallygood.co.il>
2020-08-20 10:31:12 +0300

    Adds `React.memo()` to charts

--- src/components/common/AnalyticsCharts/QuizCompletionRateChart/index.tsx
+++ src/components/common/AnalyticsCharts/QuizCompletionRateChart/index.tsx
@@ -12,63 +12,64 @@ import { calculatePercentages } from '../utils';
 
 import QuizCompletionRateLegend from './QuizCompletionRateLegend';
 
-export default function QuizCompletionRateChart({
-  className,
-  title,
-  overview,
-  isLoading = false,
-}: IQuizCompletionRateChart): ReactElement {
-  const [totalPercentages, setTotalPercentages] = useState<number>();
-  const [bars, setBars] = useState<IBar[]>([]);
+const QuizCompletionRateChart = React.memo(
+  ({ className, title, overview, isLoading = false }: IQuizCompletionRateChart): ReactElement => {
+    const [totalPercentages, setTotalPercentages] = useState<number>();
+    const [bars, setBars] = useState<IBar[]>([]);
 
-  useEffect(() => {
-    if (overview) {
-      const { users_passed, users_submitted } = overview;
+    useEffect(() => {
+      if (overview) {
+        const { users_passed, users_submitted } = overview;
 
-      if (users_passed && users_submitted)
-        setTotalPercentages(calculatePercentages(users_passed, users_submitted));
-    }
-  }, [overview]);
+        if (users_passed && users_submitted)
+          setTotalPercentages(calculatePercentages(users_passed, users_submitted));
+      }
+    }, [overview]);
 
-  useEffect(() => {
-    if (totalPercentages)
-      setBars([
-        {
-          value: totalPercentages,
-          legend: 'Users who completed a course',
-        },
-      ]);
-  }, [totalPercentages]);
+    useEffect(() => {
+      if (totalPercentages)
+        setBars([
+          {
+            value: totalPercentages,
+            legend: 'Users who completed a course',
+          },
+        ]);
+    }, [totalPercentages]);
 
-  // unmount only
-  useEffect(
-    () => () => {
-      setTotalPercentages(0);
-      setBars([]);
-    },
-    [],
-  );
+    // unmount only
+    useEffect(
+      () => () => {
+        setTotalPercentages(0);
+        setBars([]);
+      },
+      [],
+    );
 
-  return (
-    <WMCard title={title}>
-      <WMSkeleton loading={isLoading} active paragraph={{ rows: 2 }}>
-        <div className={className}>
-          {totalPercentages ? (
-            <>
-              <PieBarSummary
-                value={totalPercentages as number}
-                unit="%"
-                text={` (${overview?.users_passed ?? 0} of ${
-                  overview?.users_submitted ?? 0
-                } users)`}
-              />
-              <PieBarChart bars={bars} legendContent={QuizCompletionRateLegend} />
-            </>
-          ) : (
-            <CourseQuizEmptyState />
-          )}
-        </div>
-      </WMSkeleton>
-    </WMCard>
-  );
-}
+    return (
+      <WMCard title={title}>
+        <WMSkeleton loading={isLoading} active paragraph={{ rows: 2 }}>
+          <div className={className}>
+            {totalPercentages ? (
+              <>
+                <PieBarSummary
+                  value={totalPercentages as number}
+                  unit="%"
+                  text={` (${overview?.users_passed ?? 0} of ${
+                    overview?.users_submitted ?? 0
+                  } users)`}
+                />
+                <PieBarChart bars={bars} legendContent={QuizCompletionRateLegend} />
+              </>
+            ) : (
+              <CourseQuizEmptyState />
+            )}
+          </div>
+        </WMSkeleton>
+      </WMCard>
+    );
+  },
+);
+
+QuizCompletionRateChart.displayName = 'QuizCompletionRateChart';
+
+export default QuizCompletionRateChart;
