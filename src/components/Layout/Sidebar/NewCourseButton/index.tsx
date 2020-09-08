d7c35fd71dbb286caa6bead4ea7928507add3bd6
Denise Rosegg <denise@reallygood.co.il>
2020-09-08 14:40:16 +0300

    Reloads Course Editor page when the user is already on that page and they click on the 'new course' button.

--- src/components/Layout/Sidebar/NewCourseButton/index.tsx
+++ src/components/Layout/Sidebar/NewCourseButton/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
 import { NEW_COURSE_EDITOR_ROUTE } from '../../../../constants/routes';
 import WMButton, { ButtonVariantEnum } from '../../../common/WMButton';
@@ -8,8 +9,23 @@ import WMButton, { ButtonVariantEnum } from '../../../common/WMButton';
 import classes from './style.module.scss';
 
 export default function NewCourseButton(): ReactElement {
+  const history = useHistory();
+  const { pathname: currentPath } = useLocation();
+
+  const onLinkClick = (e: React.MouseEvent<HTMLElement>) => {
+    if (NEW_COURSE_EDITOR_ROUTE.path === currentPath) {
+      e.preventDefault();
+
+      history.replace({ key: uuidv4(), pathname: NEW_COURSE_EDITOR_ROUTE.path });
+    }
+  };
+
   return (
-    <Link className={classes['create-button-wrapper']} to={NEW_COURSE_EDITOR_ROUTE.path}>
+    <Link
+      className={classes['create-button-wrapper']}
+      to={NEW_COURSE_EDITOR_ROUTE.path}
+      onClick={onLinkClick}
+    >
       <WMButton
         shape="round"
         variant={ButtonVariantEnum.Create}
